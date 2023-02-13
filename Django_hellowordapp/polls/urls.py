a5e6286196c5a489889090d63412970231388f90
joankenobi <joan1233blanco@gmail.com>
2023-02-13 07:02:01 -0400

    1 empezando con las views simples, sin templates

--- Django_hellowordapp/polls/urls.py
+++ Django_hellowordapp/polls/urls.py
@@ -4,5 +4,15 @@ from . import views
 urlpatterns=[
     path(route="",
          view=views.index,
-         name="index") # se aplica lo descrito en urls.py de la app principal
+         name="index"), # se aplica lo descrito en urls.py de la app principal
+    path(route="<int:question_id>/", #<> indica el parametro que le va a llegar a la view funtion
+         view=views.detail,
+         name="detail"),
+    path(route="<int:question_id>/results/",
+         view=views.results,
+         name="results"),
+    path(route="<int:question_id>/vote/",
+         view=views.vote,
+         name="vote")
 ]
+
--- Django_hellowordapp/polls/views.py
+++ Django_hellowordapp/polls/views.py
@@ -2,6 +2,40 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    "permite generar una vista"
-    return HttpResponse("Hola Joan")
+def index(request): #la vista principal casi siempre es nombrada Index (buenas practicas)
+    "Muestra la pagina principal"
+    return HttpResponse("Hola estas en la pagina principal de peliculas questions")
+
+def detail(request, question_id)->HttpResponse:
+    """
+        Muestra las diferentes preguntas y opciones para votar.
+        
+        Parameters
+        ---
+            request: 
+            question_id: primary key of Quetion objects
+    """
+    return HttpResponse(f"You are viewing the question number{question_id}")
+
+def results(request, question_id)->HttpResponse:
+    """
+        Muestra los resultados de la votacion.
+        
+        Parameters
+        ---
+            request: 
+            question_id: primary key of Quetion objects
+    """
+    return HttpResponse(f"You are viewing the results of question number{question_id}")
+
+def vote(request, question_id)->HttpResponse:
+    """
+        Muestra que pregunta esta votando.
+        
+        Parameters
+        ---
+            request: 
+            question_id: primary key of Quetion objects
+    """
+    return HttpResponse(f"You are voting to question number{question_id}")
+
