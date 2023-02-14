c49a15e94784eb0aedd352b59ed21526cecc7f1c
joankenobi <joan1233blanco@gmail.com>
2023-02-13 21:13:37 -0400

    2 add functions views

--- Django_hellowordapp/polls/views.py
+++ Django_hellowordapp/polls/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render #conecta la view con el template
+from django.shortcuts import get_object_or_404 ,get_list_or_404 #genera un proceso Question.objects.get(), que al incumplir retorna 404
 from django.http import HttpResponse
+from .models import Question, Choice
 # Create your views here.
 
-def index(request): #la vista principal casi siempre es nombrada Index (buenas practicas)
-    "Muestra la pagina principal"
-    return HttpResponse("Hola estas en la pagina principal de peliculas questions")
+def index(request)->HttpResponse: #la vista principal casi siempre es nombrada Index (buenas practicas)
+    """Muestra la pagina principal"""
+    latest_question_list=Question.objects.all()
+    return render(request=request,
+                  template_name="polls/index.html", #se juntan todo el contenido de las carpetas templates y se escoge la carpeta polls.
+                  context={
+                      "latest_question_list":latest_question_list
+                  })
 
 def detail(request, question_id)->HttpResponse:
     """
@@ -12,10 +19,13 @@ def detail(request, question_id)->HttpResponse:
         
         Parameters
         ---
-            request: 
+            request: request
             question_id: primary key of Quetion objects
     """
-    return HttpResponse(f"You are viewing the question number{question_id}")
+    question=get_object_or_404(Question, pk=question_id)
+    return render(request=request, template_name="polls/detail.html",context={
+        "question":question
+    })
 
 def results(request, question_id)->HttpResponse:
     """
